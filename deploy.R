d6d09c57360d430ca7212595ec1af3ec84bacb75
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-08-25 12:06:21 +0100

    Deploy script

---
+++ deploy.R
@@ -0,0 +1,58 @@
+# load packages ----------------------------------------------------------------
+
+library(fs)
+library(rsconnect)
+library(tidyverse)
+library(glue)
+library(rmarkdown)
+library(shiny)
+
+# check all indexes compile -------------------------------------------------------
+
+indexes <- dir_info(recurse = 2, glob = "*x.Rmd") %>% pull(path)
+walk(indexes, render)
+
+# check all tutorials compile --------------------------------------------------
+
+rmds      <- dir_info(recurse = 2, glob = "*.Rmd")
+indexes   <- dir_info(recurse = 2, glob = "*x.Rmd")
+tutorials <- anti_join(rmds, indexes, by = "path") %>% pull(path)
+# walk(tutorials, run) -- doesn't work, need a way to shut down each tutorial
+
+# get a list of directories ----------------------------------------------------
+
+tutorial_dirs <- dir_info(recurse = 1) %>%
+  filter(
+    type == "directory",
+    str_detect(path, "/"),
+    str_detect(path, "lesson")
+    ) %>%
+  pull(path)
+
+# tutorials to deploy ----------------------------------------------------------
+
+which_tutorials <- 1:8
+which_tutorials_chr <- paste0("0", which_tutorials)
+which_tutorials_regex <-paste0("^", which_tutorials_chr, collapse = "|")
+dirs_to_deploy <- str_subset(tutorial_dirs, which_tutorials_regex)
+
+# lessons ----------------------------------------------------------------------
+
+tutorials <- tibble(dir_to_deploy = dirs_to_deploy, lesson = dirs_to_deploy) %>%
+  separate(col = lesson, into = c("tutorial", "lesson"), sep = "/") %>%
+  mutate(lesson = str_remove(lesson, "-lesson")) %>%
+  mutate(title = glue("ims-{tutorial}-{lesson}"))
+
+
+# deploy all -------------------------------------------------------------------
+
+for(i in 1:length(dirs_to_deploy)){
+#for(i in 19:22){
+  deployApp(
+    appDir = tutorials$dir_to_deploy[i],
+    appTitle = tutorials$title[i],
+    account = "openintro",
+    forceUpdate = TRUE,
+    launch.browser = FALSE
+  )
+}
