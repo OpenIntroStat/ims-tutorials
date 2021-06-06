0b4c84d4d5e443b56c5bf4ff85b836ec86ed8db0
Mine Cetinkaya-Rundel <cetinkaya.mine@gmail.com>
2021-06-06 21:32:35 +0100

    Match organization for IMS1 (#60)
    * 01-data tutorials
    
    * 02-explore tutorials
    
    * All new addresses for tutorials
    
    * Update links from indices
    
    * Update README
    
    * Adjust deploy script

--- deploy.R
+++ deploy.R
@@ -31,7 +31,7 @@ tutorial_dirs <- dir_info(recurse = 1) %>%
 
 # tutorials to deploy ----------------------------------------------------------
 
-which_tutorials <- 1:8
+which_tutorials <- 6
 which_tutorials_chr <- paste0("0", which_tutorials)
 which_tutorials_regex <-paste0("^", which_tutorials_chr, collapse = "|")
 dirs_to_deploy <- str_subset(tutorial_dirs, which_tutorials_regex)
@@ -43,11 +43,10 @@ tutorials <- tibble(dir_to_deploy = dirs_to_deploy, lesson = dirs_to_deploy) %>%
   mutate(lesson = str_remove(lesson, "-lesson")) %>%
   mutate(title = glue("ims-{tutorial}-{lesson}"))
 
-
 # deploy all -------------------------------------------------------------------
 
-for(i in 1:length(dirs_to_deploy)){
-#for(i in 19:22){
+#for(i in 1:length(dirs_to_deploy)){
+for(i in 3:5){
   deployApp(
     appDir = tutorials$dir_to_deploy[i],
     appTitle = tutorials$title[i],
